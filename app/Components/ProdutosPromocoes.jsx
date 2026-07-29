83f5e17e84f23e417b2ec16ba9f2f58650e7d713
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-07-29 10:22:51 -0300

    atualizar

--- app/Components/ProdutosPromocoes.jsx
+++ app/Components/ProdutosPromocoes.jsx
@@ -4,15 +4,18 @@ export default function ProdutosPromoções(){
     return(
         <>
             {Data.map((item) => {
-                return(
-                    <div key={item.Nome}>
+                if(item.Promoção==true){
+
+                    return(
+                        <div key={item.Nome}>
                         <img src={item.img} alt="" />
                         <h1>Nome: {item.Nome}</h1>
                         <p>Descrição:{item.Descrição}</p>
                         <p>Preço:{item.Preço}</p>
-                        
+
                     </div>
                 )
+            }
             })}
         </>
     )
