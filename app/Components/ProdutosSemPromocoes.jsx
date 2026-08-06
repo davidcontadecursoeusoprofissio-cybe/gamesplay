35818eeb767d6d12a926ae64a897529e4b7a9cdc
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-08-06 09:17:44 -0300

    atualiza

--- app/Components/ProdutosSemPromocoes.jsx
+++ app/Components/ProdutosSemPromocoes.jsx
@@ -1,15 +1,19 @@
 import Link from "next/link"
 import Data from "../Data/Data.json"
 
-export default function ProdutosSemPromoções(){
-    return(
-        {Data.map((item)=>{
-            return(
-                <div key={item}>
-                
-                </div>
-            )
-        })}
-
+export default function ProdutosSemPromoções() {
+    return (
+        <>
+            {Data.map((item) => {
+                return (
+                    <div key={item.Nome}>
+                        <img src={item.img} alt="" />
+                        <h1>Nome: {item.Nome}</h1>
+                        <p>Descrição: {item.Descrição}</p>
+                        <p>Preço: {item.Preço}</p>
+                    </div>
+                )
+            })}
+        </>
     )
 }
